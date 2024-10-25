c9f2a01ea5d1297bcfef95bab1e8ec8ab7df521c
DrFunFacts <bertruam@gmail.com>
2024-10-25 09:21:49 -0500

    10/25/24 V0.0.3

--- wizard_game.py
+++ wizard_game.py
@@ -16,6 +16,8 @@ game_title = """          _    __________________ _______  _______ _________ ___
                                                                                                                                                                                               
 an ultimate power simulator by Kiefer Noles                                                                                                                                                                                              \n\n\n"""
 
+support_link = "https://ko-fi.com/knolesofnme"
+
 #List of conditions for player confirmation
 pos_cond = ["yes", "y", "yes", "yse"]
 neg_cond = ["no", "n", "on"]
@@ -30,6 +32,8 @@ current_foe = Monster
 game_stat = Game_Status
 #/Game Class Object
 
+log = []
+
 def die_roll(power):
     global game_stat
     result = 0
@@ -43,22 +47,12 @@ def die_roll(power):
     dice_log(log_pass)
     return result
 
-def threat_gen(power):
-    result = 0
-    for x in range(power):
-        add = random.randrange(1,6)
-        result += add
-    if result == 1:
-        result = 2
-    if result == power * 6:
-        result = power * 6 - 1
-    return result
-
+#Logs rolls for later review
 def dice_log(x):
     print(x)
-    log = []
     log.append(x)
 
+#Creates a new wizard for a new game
 def wizard_maker():
     char_finished = False
     name_check = False
@@ -111,6 +105,7 @@ def wizard_maker():
     
     return Wizard(name, title, job, school)
 
+#Setup
 def main():
     os.system('clear')
     print(game_title)
@@ -120,8 +115,9 @@ def main():
     f.close()
     menu()
 
+#Main Menu
 def menu():
-    print("Welcome to your Ultimate Wizard Journey!\n   1) New Game\n   2) Tutorial\n   3) High Scores\n   4) Quit\n")
+    print("Welcome to your Ultimate Wizard Journey!\n   1) New Game\n   2) Tutorial\n   3) High Scores\n    4) Support the Creator\n    5) Quit\n")
     menu_check = False    
     while menu_check == False:
         menu_in = input("Please enter the number of the desired command: ")
@@ -140,10 +136,13 @@ def menu():
                 print(f"{f.read()}\n")
                 menu()
             case "4":
+                print(f"If you would like to support this and future projects, you can do so here: {support_link}")
+            case "5":
                 end_program()
             case _:
                 print("Command Not Found")
 
+#Sets the difficulty for the game
 def choose_difficulty():
     print("What difficulty?\n   1) Easy\n   2) Normal\n   3) Hard\n   4) Painful\n")
     diff_check = False    
@@ -169,18 +168,35 @@ def choose_difficulty():
             case _:
                 print("Command Not Found")
 
+#sets up a new game
 def new_game():
     #make a new wizard
     global current_wizard
     global game_stat
+    global log
+    log = []
     current_wizard = wizard_maker()
     game_stat = choose_difficulty()
     game_loop()
 
+#Generate an enemy for the game
 def make_monster():
     global current_foe 
     current_foe = Monster(threat_gen(game_stat.power))
 
+#sets the monsters difficulty
+def threat_gen(power):
+    result = 0
+    for x in range(power):
+        add = random.randrange(1,6)
+        result += add
+    if result == 1:
+        result = 2
+    if result == power * 6:
+        result = power * 6 - 1
+    return result
+
+#Basic gameplay
 def game_loop():
     print(f"\nDay {game_stat.r + 1}\n")
     time.sleep(random.randrange(1, 3))
@@ -206,18 +222,61 @@ def game_loop():
         if spectator == victory:
             battle_check = True
 
-# returns current health, current score, current rerolls
+#Checks to see who wins the fight
+def battle_system(tact):
+    checkem  = 0
+    checkem = die_roll(game_stat.power)
+    #subtlety
+    if (tact == True and checkem < current_foe.threat) or (tact == False and checkem > current_foe.threat):
+        victory()
+        return "victory"
+    elif checkem == current_foe.threat:
+        print("You clash spectacularly!")
+    else:
+        if game_stat.rerolls > 0:
+            rerolling = input(f"You rolled a {checkem} Do you wish to reroll? ")
+            if rerolling in pos_cond:
+                game_stat.rerolls -= 1
+                battle_system(tact)
+            else:
+                injury()
+        else:
+            injury()
+
+# prints current health, current score, current rerolls.
 def status_check():
     print(f"Status: Health - {game_stat.health}/{game_stat.maxhealth} Score - {game_stat.score} Rerolls - {game_stat.rerolls}")
 
-def entomb(obit, comment):
-    f = open("Masoleum.txt", "a")
-    if comment != "":
-        f.write(f"\n Here lies {obit}\n'{comment}'")
+#Called when an enemy is defeated.
+def victory():
+    print(victory_text[random.randrange(len(victory_text))])
+    game_stat.score += 1
+    print("Current Score: " + str(game_stat.score))
+    if (game_stat.score % 10) == 0:
+        power_increses()
     else:
-        f.write(f"\n Here lies {obit}")
-    f.close
+        game_loop()
 
+#Increases the power of the game every ten points scored.
+def power_increses():
+    game_stat.power += 1
+    print(power_increase_text)
+    if(game_stat.health < game_stat.maxhealth):
+        print("Thoust heals one wound!\n")
+        game_stat.health += 1
+    print("Thoust hast earned a reroll!")
+    game_stat.rerolls += 1
+    time.sleep(2)
+    game_loop()
+
+#reduces player health
+def injury():
+    print("Thou are harmed!")
+    game_stat.health -= 1
+    if game_stat.health <= 0:
+        game_over()
+
+#Called when the player health is reduced to 0
 def game_over():
     print("Deathblow!")
     obituary = f"Here lies {current_wizard.true_name} {game_over_text} a {current_foe.name} on day {game_stat.score + 1} of their journey!"
@@ -240,54 +299,19 @@ def game_over():
     #high score check goes here
     menu()
 
+# Commits the character to the highscore system
+def entomb(obit, comment):
+    f = open("Masoleum.txt", "a")
+    if comment != "":
+        f.write(f"\n Here lies {obit}\n'{comment}'")
+    else:
+        f.write(f"\n Here lies {obit}")
+    f.close
+
+#Closes the game
 def end_program():
     print("Fare thee well!")
     quit()
 
-def power_increses():
-    game_stat.power += 1
-    print(power_increase_text)
-    if(game_stat.health < game_stat.maxhealth):
-        print("Thoust heals one wound!\n")
-        game_stat.health += 1
-    print("Thoust hast earned a reroll!")
-    game_stat.rerolls += 1
-    time.sleep(2)
-    game_loop()
-
-def battle_system(tact):
-    checkem  = 0
-    checkem = die_roll(game_stat.power)
-    #subtlety
-    if (tact == True and checkem < current_foe.threat) or (tact == False and checkem > current_foe.threat):
-        victory()
-        return "victory"
-    elif checkem == current_foe.threat:
-        print("You clash spectacularly!")
-    else:
-        if game_stat.rerolls > 0:
-            rerolling = input(f"You rolled a {checkem} Do you wish to reroll? ")
-            if rerolling in pos_cond:
-                game_stat.rerolls -= 1
-                battle_system(tact)
-            else:
-                injury()
-        else:
-            injury()
-
-def victory():
-    print(victory_text[random.randrange(len(victory_text))])
-    game_stat.score += 1
-    print("Current Score: " + str(game_stat.score))
-    if (game_stat.score % 10) == 0:
-        power_increses()
-    else:
-        game_loop()
-
-def injury():
-    print("Thou are harmed!")
-    game_stat.health -= 1
-    if game_stat.health <= 0:
-        game_over()
-
+#Runs the game when called
 main()
